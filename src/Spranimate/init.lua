b04f82a99e81b5f095ce2c3051a0fba72e359dd8
paige <33643911+presssssure@users.noreply.github.com>
2021-03-20 02:26:43 -0400

    update structure

---
+++ src/Spranimate/init.lua
@@ -0,0 +1,5 @@
+return {
+    Spranimation = require(script:WaitForChild("Spranimation")),
+    SpranimationTrack = require(script:WaitForChild("SpranimationTrack")),
+    Spranimator = require(script:WaitForChild("Spranimator")),
+}
