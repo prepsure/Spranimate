4b5098cf8c877dccabeaf29a32051622237c4fdd
paige <33643911+presssssure@users.noreply.github.com>
2021-03-22 14:35:04 -0400

    declare coroutine in constructor

--- src/Spranimate/SpranimationTrack.lua
+++ src/Spranimate/SpranimationTrack.lua
@@ -23,6 +23,12 @@ function SpranimationTrack.new(Spranimation, Spranimator)
     self.Speed = 1
     self.TimePosition = 0
 
+    self._playThread = coroutine.wrap(function()
+        while true do
+            self:_playSpranimation()
+        end
+    end)
+
     return self--[[setmetatable({}, {
         __index = self,
         __newindex = function(index, value)
@@ -33,6 +39,24 @@ function SpranimationTrack.new(Spranimation, Spranimator)
             self[index] = value
         end
     })]]
+end
+
+
+function SpranimationTrack:_playSpranimation()
+
+    for i, segment in pairs(self.Spranimation._segmentTable) do
+        local framesInSegment = segment.EndFrame - segment.StartFrame + 1
+
+        for frame = segment.StartFrame, segment.EndFrame do
+            self._spranimator:SetFrame(frame)
+            FastWait(segment.Length/framesInSegment)
+
+            if not self.IsPlaying then
+                coroutine.yield()
+            end
+        end
+    end
+
 end
 
 
@@ -52,25 +76,7 @@ end
 
 
 function SpranimationTrack:Play(speed)
-
-    self._playing = coroutine.wrap(function()
-
-        while true do
-            local spranimation = self.Spranimation
-
-            for i, segment in pairs(spranimation._segmentTable) do
-                local framesInSegment = segment.EndFrame - segment.StartFrame + 1
-
-                for frame = segment.StartFrame, segment.EndFrame do
-                    self._spranimator:SetFrame(frame)
-                    FastWait(segment.Length/framesInSegment)
-                end
-            end
-        end
-
-    end)
-
-    self._playing()
+    self._playThread()
 end
 
 
