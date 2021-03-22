9d60b755be76472a42e4b3a071195b83ed98470b
paige <33643911+presssssure@users.noreply.github.com>
2021-03-22 03:19:16 -0400

    add setframe function

--- src/Spranimate/Spranimator.lua
+++ src/Spranimate/Spranimator.lua
@@ -1,3 +1,6 @@
+local SpranimationTrack = require(script.Parent.SpranimationTrack)
+
+
 local Spranimator = {}
 
 Spranimator.__index = Spranimator
@@ -11,11 +14,26 @@ function Spranimator.new(gui)
     self.ClassName = "Spranimator"
     self.Name = "Spranimator"
 
+    self.SpriteSize = gui:GetAttribute("SpriteSize")
+    self.ImageSize = gui:GetAttribute("ImageSize")
+
     self.Changed = Instance.new("BindableEvent")
     self.AnimationPlayed = Instance.new("BindableEvent")
 
     return self
 
+end
+
+
+function Spranimator:SetFrame(frame)
+    local zeroIndexed = frame - 1
+
+    local spritesPerRow = self.ImageSize.X/self.SpriteSize.X
+
+    local col = zeroIndexed % spritesPerRow
+    local row = math.floor(zeroIndexed / spritesPerRow)
+
+    self.Adornee.ImageRectOffset = Vector2.new(col * self.SpriteSize.X, row * self.SpriteSize.Y)
 end
 
 
@@ -25,7 +43,7 @@ end
 
 
 function Spranimator:LoadSpranimation(Spranimation)
-    return SpranimationTrack.new(Spranimation)
+    return SpranimationTrack.new(Spranimation, self)
 end
 
 
