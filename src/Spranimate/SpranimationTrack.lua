dfc8322c4cf0bca4399cb87fdf0670cb6d26a42a
paige <33643911+presssssure@users.noreply.github.com>
2021-03-25 01:47:30 -0400

    comment code

--- src/Spranimate/SpranimationTrack.lua
+++ src/Spranimate/SpranimationTrack.lua
@@ -14,7 +14,7 @@ function SpranimationTrack.new(Spranimation, Spranimator)
     local self = setmetatable({}, SpranimationTrack)
 
     self.Spranimation = Spranimation
-    self._spranimator = Spranimator
+    self._spranimator = Spranimator -- just used for setting the frame, so can be private
 
     self.Looped = Spranimation.Looped
     self.Priority = Spranimation.Priority
@@ -27,7 +27,7 @@ function SpranimationTrack.new(Spranimation, Spranimator)
     self.CurrentSegmentIndex = 1
 
     self._destroyed = false
-    self._playThread = self:_loopWithTimings()
+    self._playThread = self:_makeSpranimationCoroutine()
     self._segmentSignalTable = {}
 
     return self--[[setmetatable({}, {
@@ -43,14 +43,18 @@ function SpranimationTrack.new(Spranimation, Spranimator)
 end
 
 
+-- a function that sets the animation to the next frame in its sequence, respecting segments
 function SpranimationTrack:AdvanceFrame()
     local segTable = self.Spranimation._segmentTable
     local segment = segTable[self.CurrentSegmentIndex]
 
+    -- if we're at the end of the segment, we have to jump to the next one
     if self.CurrentFrame == segment.EndFrame then
+        -- get to the next segment using mods to loop back to the first if needed
         self.CurrentSegmentIndex = (self.CurrentSegmentIndex % #segTable) + 1
         self.CurrentFrame = segment.StartFrame
 
+        -- if the user has a signal attatched to the loading of the new segment, it should fire
         local segSignal = self._segmentSignalTable[segment.Name]
         if segSignal then
             segSignal:Fire(segment.Name)
@@ -63,19 +67,25 @@ function SpranimationTrack:AdvanceFrame()
 end
 
 
-function SpranimationTrack:_loopWithTimings()
+function SpranimationTrack:_makeSpranimationCoroutine()
     return coroutine.wrap(function()
+        -- coroutine will only complete upon the instance being destroyed
         while not self._destroyed do
+
             if not self.IsPlaying then
+                -- pause when not playing
                 coroutine.yield()
             end
 
+            -- set next frame
             self:AdvanceFrame()
 
+            -- wait for next frame (timing must be >=1/60 of a second)
             local segTable = self.Spranimation._segmentTable
             local segment = segTable[self.CurrentSegmentIndex]
-            local framesInSegment = segment.EndFrame - segment.StartFrame + 1
-            FastWait(segment.Length/framesInSegment/self.Speed)
+            local framesInSegment = segment.EndFrame - segment.StartFrame + 1 -- inclusive countining :)
+            FastWait(segment.Length/framesInSegment/self.Speed) -- TODO a negative speed should play the animation backwards
+
         end
     end)
 end
